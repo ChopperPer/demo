a0e91e89a53596e3b1590f530d6063cdbb27cb2f
ChopperPer <chopperper@gmail.com>
2020-05-20 08:19:16 +0200

    Added get endoints to API

--- src/main/java/se/frosochopper/demo/control/SongController.java
+++ src/main/java/se/frosochopper/demo/control/SongController.java
@@ -1,6 +1,7 @@
 package se.frosochopper.demo.control;
 
 import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping(path = "/api/v1/songs")
@@ -8,4 +9,38 @@ public class SongController {
 
     @GetMapping("/ping")
     public String ping() { return "Pong"; }
+    
+    @GetMapping("/")
+    public ArrayList<String> getAll(){
+        // TODO: Swop String to SongModel when it exists
+        // TODO: implement songService
+        // return songService.getAll();
+        
+        //Temp mock
+        private ArrayList<String> songs = new ArrayList<String>();
+        songs.add("Fly me to the moon");
+        songs.add("Star man");
+        return songs;
+    }
+
+    @GetMapping("/{id}")
+    public String getMessageModel(@PathVariable("id") Long id){
+        // TODO: Swop String to SongModel when it exists
+        // TODO: implement songService
+        // return songService.getMessageModel(id);
+        
+        // Temp mock
+        String song = "";
+        switch(id) {
+          case 1:
+            song = "Fly me to the moon";
+            break;
+          case 2:
+            song = "Starman";
+            break;
+          default:
+            song = "No song found";
+        }
+        return song;
+    }
 }
