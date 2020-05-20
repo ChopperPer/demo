a4adc6eb30148d7c87af6bc45287adb17c1a7414
ChopperPer <chopperper@gmail.com>
2020-05-20 07:45:57 +0200

    Removed SongController from wrong position

--- SongController.java
+++
@@ -1,9 +0,0 @@
-package se.frosochopper.demo.control;
-
-@RestController
-@RequestMapping(path = "/api/v1/songs")
-public class SongController {
-
-    @GetMapping("/ping")
-    public String ping() { return "Pong"; }
-}
